901549e6928311330e42663355d7d0262a50de15
Micha Alt <mail@lean-stack.rocks>
2019-11-19 23:42:45 +0100

    Created and used a first class component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
 import React from 'react';
 
 import './App.css';
+import KanbanBoard from './components/KanbanBoard.jsx';
 
-const App = () => (
-  <div className="teaser">
-    <header>
-      <h2>The Webpack Toolchain</h2>
-    </header>
-    <div className="kanban">
-      <article>
-        <h3>To Do</h3>
-        <ul>
-          <li>Typescript</li>
-          <li>Testing</li>
-          <li>CSS Präpozessoren bzw. PostCSS</li>
-          <li>Code Splitting</li>
-        </ul>
-      </article>
-      <article>
-        <h3>In Progress</h3>
-        <ul>
-        </ul>
-      </article>
-      <article>
-        <h3>Done</h3>
-        <ul>
-          <li>Bundling</li>
-          <li>HTML ausliefern</li>
-          <li>JSX Transpilation</li>
-          <li>Styles Handling</li>
-        </ul>
-      </article>
-    </div>
-  </div>
-);
+const App = () => <KanbanBoard />;
 
 export default App;
---
+++ src/components/KanbanBoard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default class KanbanBoard extends React.Component {
+  render() {
+    return (
+      <div className="teaser">
+        <header>
+          <h2>The Webpack Toolchain</h2>
+        </header>
+        <div className="kanban">
+          <article>
+            <h3>To Do</h3>
+            <ul>
+              <li>Typescript</li>
+              <li>Testing</li>
+              <li>CSS Präpozessoren bzw. PostCSS</li>
+              <li>Code Splitting</li>
+            </ul>
+          </article>
+          <article>
+            <h3>In Progress</h3>
+            <ul></ul>
+          </article>
+          <article>
+            <h3>Done</h3>
+            <ul>
+              <li>Bundling</li>
+              <li>HTML ausliefern</li>
+              <li>JSX Transpilation</li>
+              <li>Styles Handling</li>
+            </ul>
+          </article>
+        </div>
+      </div>
+    );
+  }
+}
